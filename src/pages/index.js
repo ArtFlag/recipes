f7d1e019521f6f018e4e66d1684faff3faa9152a
ArtFlag <aflageul@tuta.io>
2021-03-20 11:13:26 +0100

    Add button for bread

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,6 +87,13 @@ function Home() {
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
+
+            <Link
+                className={clsx("button button--outline button--secondary button--lg", styles.getStarted, styles.button)}
+                to={useBaseUrl("docs/bread/pain")}
+              >
+                Bread
+            </Link>
             <Link
               className={clsx("button button--outline button--secondary button--lg", styles.getStarted, styles.button)}
               to={useBaseUrl("docs/savoury/adjaruli")}
